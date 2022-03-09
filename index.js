7847d88e20f5b6e18e65f8c60f5bf8cb0c1a3f37
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-09 17:31:05 +0000

    basic routing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use('/about', express.static('public/about.html'));
 
-app.get('/', (req, res) => {
-    res.send('test');
-})
+// app.get('/', (req, res) => {
+//     res.send('test');
+// })
 
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
