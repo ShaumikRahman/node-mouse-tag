586f21a7d999345838fba7c38289e156e3eb275c
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-11 14:49:23 +0000

    sass setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 app.use('/about', express.static('public/about.html'));
 
 // app.get('/', (req, res) => {
