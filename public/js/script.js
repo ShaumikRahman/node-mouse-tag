7c69993e215e332ad21c6f2287d7ca2c0f8a2256
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-16 23:24:00 +0000

    added clickable

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,10 @@
 const coords = document.getElementById('coords');
+const body = document.getElementById('body');
 
 window.onmousemove = (e) => {
     coords.textContent = `${e.clientX} x ${e.clientY}`;
+}
+
+window.onclick = (e) => {
+    body.classList.toggle('clickable');
 }
