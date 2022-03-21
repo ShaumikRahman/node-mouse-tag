99d45a393af27390a707afd57aec4b821e8d3a4b
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-21 21:31:51 +0000

    include text toggle on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 const coords = document.getElementById('coords');
 const body = document.getElementById('body');
+const texts = document.getElementsByClassName('text');
 
 window.onmousemove = (e) => {
     coords.textContent = `${e.clientX} x ${e.clientY}`;
@@ -7,4 +8,7 @@ window.onmousemove = (e) => {
 
 window.onclick = (e) => {
     body.classList.toggle('clickable');
+    for (let i = 0; i < texts.length; i++) {
+        texts[i].classList.toggle('hide');
+    }
 }
