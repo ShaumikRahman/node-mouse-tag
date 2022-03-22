ec58dee221ea030a3b3da0a7516422c84df504de
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-22 18:44:56 +0000

    added absolute canvas

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,18 @@ const coords = document.getElementById('coords');
 const body = document.getElementById('body');
 const texts = document.getElementsByClassName('text');
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 window.onmousemove = (e) => {
-    coords.textContent = `${e.clientX} x ${e.clientY}`;
+    coords.textContent = `${e.clientX} x ${e.clientY}`;    
+}
+
+window.onresize = (e) => {
+    console.log(`${canvas.width} x ${canvas.height}`);
 }
 
 window.onclick = (e) => {
