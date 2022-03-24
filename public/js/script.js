d28500cd05f27c77f7e48e40fec4dc5cffb68cb3
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-03-24 16:21:39 +0000

    new circle style, click to clear

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,42 @@
-const coords = document.getElementById('coords');
-const body = document.getElementById('body');
-const texts = document.getElementsByClassName('text');
+const coords = document.getElementById("coords");
+const body = document.getElementById("body");
+const texts = document.getElementsByClassName("text");
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function drawCircle(xPos, yPos) { ctx.beginPath(); ctx.arc(xPos, yPos, 50, 0, 360); ctx.moveTo(0, 0); ctx.closePath(); ctx.fillStyle = 'white'; ctx.fill(); ctx.strokeStyle = "red"; ctx.stroke();}
+function drawCircle(xPos, yPos) {
+  ctx.beginPath();
+  ctx.arc(xPos, yPos, 12, 0, 360);
+  ctx.moveTo(0, 0);
+  ctx.closePath();
+  ctx.fillStyle = "#0070f0";
+  ctx.fill();
+  ctx.strokeStyle = "0070ff";
+  ctx.stroke();
+}
 
 window.onmousemove = (e) => {
-    coords.textContent = `${e.clientX} x ${e.clientY}`;
-    drawCircle(e.clientX, e.clientY);
-}
+  coords.textContent = `${e.clientX} x ${e.clientY}`;
+  drawCircle(e.clientX, e.clientY);
+  //requestAnimationFrame(update);
+};
+
+window.addEventListener("click", (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log("test");
+});
 
 window.onresize = (e) => {
-    console.log(`${canvas.width} x ${canvas.height}`);
-}
+  console.log(`${canvas.width} x ${canvas.height}`);
+};
 
 window.onclick = (e) => {
-    body.classList.toggle('clickable');
-    for (let i = 0; i < texts.length; i++) {
-        texts[i].classList.toggle('hide');
-    }
-}
+  body.classList.toggle("clickable");
+  for (let i = 0; i < texts.length; i++) {
+    texts[i].classList.toggle("hide");
+  }
+};
