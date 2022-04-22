88b4d11df2182f31c7a0addbff26fb8ad0248555
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2022-04-22 16:11:45 +0100

    new socket code

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const path = require('path');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const socketio = require('socket.io');
+const {Server} = require('socket.io');
 const PORT = process.env.PORT || 3000;
 
-const io = socketio(server);
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on('connection', (socket) => {
+    console.log('connected', socket.id)
+})
 
 server.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
